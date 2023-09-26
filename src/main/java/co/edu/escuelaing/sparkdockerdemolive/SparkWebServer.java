b8ede4c5bb74111e457eb6a1d0ba5eeec8219a60
RiCHi <60302534+Ricardo-Olarte@users.noreply.github.com>
2023-09-25 22:40:20 -0500

    Add files via upload

--- src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
+++ src/main/java/co/edu/escuelaing/sparkdockerdemolive/SparkWebServer.java
@@ -7,6 +7,11 @@ public class SparkWebServer {
     public static void main(String... args){
         port(getPort());
         get("hello", (req,res) -> "Hello Docker!");
+        get("sin", (req,res) -> sin("45"));
+        get("cos", (req,res) -> cos("45"));
+        get("palindromo", (req,res) -> palindromo("amolapaloma"));
+        get("magnitud", (req,res) -> magnitud("2", "3"));
+
     }
 
     private static int getPort() {
@@ -14,6 +19,28 @@ public class SparkWebServer {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 4567;
+    }
+
+    private static String sin(String s){
+        return String.valueOf(Math.sin(Double.parseDouble(s)));
+    }
+
+    private static String cos(String s){
+        return String.valueOf(Math.cos(Double.parseDouble(s)));
+    }
+
+    private static String palindromo(String string){
+        String c = "";
+        for(int i=(string.length()-1);i>=0;--i){
+            c += string.charAt(i);
+        }
+        return String.valueOf(string.equals(c));
+    }
+
+    private static String magnitud(String s1, String s2){
+        Double a = Double.parseDouble(s1);
+        Double b = Double.parseDouble(s2);
+        return String.valueOf(Math.sqrt(Math.pow(b,2) + Math.pow(a,2)));
     }
 
 }
